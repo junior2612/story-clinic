5dfcef60a0f55dc6178f056c39f9c7ae76bc1bc4
junior2612 <ronaldojunior2612@gmail.com>
2025-10-21 16:37:39 -0300

    Delete app/login/route.ts

--- app/login/route.ts
+++
@@ -1,2 +0,0 @@
-import { NextResponse, NextRequest } from "next/server";
-export function GET(req:NextRequest){ const url = req.nextUrl.clone(); url.pathname='/entrar'; return NextResponse.redirect(url); }
